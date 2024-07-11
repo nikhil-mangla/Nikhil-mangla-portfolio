2278f7b42e84818d3e60cf0298595e52d7a5bcd2
nikhil-mangla <142961963+nikhil-mangla@users.noreply.github.com>
2024-07-11 15:52:23 +0530

    contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,27 +1,27 @@
-// function sendMail() {
-//   // Get the values from the form elements
-//   var params = {
-//     name: document.getElementById("name").value,
-//     email: document.getElementById("email").value,
-//     message: document.getElementById("message").value
-//   };
+function sendMail() {
+  // Get the values from the form elements
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
 
-//   // Define the EmailJS service and template IDs
-//   const serviceID = "service_yhraazh";
-//   const templateID = "template_226ikmm";
+  // Define the EmailJS service and template IDs
+  const serviceID = "service_yhraazh";
+  const templateID = "template_226ikmm";
 
-//   // Use EmailJS to send the email
-//   emailjs
-//     .send(serviceID, templateID, params)
-//     .then((res) => {
-//       // Clear the form fields on successful email send
-//       document.getElementById("name").value = "";
-//       document.getElementById("email").value = "";
-//       document.getElementById("message").value = "";
-//       console.log(res);
-//       alert("Your message was sent successfully");
-//     })
-//     .catch((err) => console.log(err));
+  // Use EmailJS to send the email
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      // Clear the form fields on successful email send
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message was sent successfully");
+    })
+    .catch((err) => console.log(err));
 
-// }
+}
 
